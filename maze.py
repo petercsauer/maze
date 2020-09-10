c9452d6172913cf2755825ee1802c00bbe44614c
Peter Sauer <petercsauer@me.com>
2020-09-10 11:52:41 -0500

    9/10/2020

--- maze.py
+++ maze.py
@@ -7,14 +7,14 @@ n = 5
 def addTwohopNeighbor(x,y,m,n,f):
     
     #x+2, x-2, y+2, y-2, (y+1,x+1),(y-1,x-1),(y+1,x-1),(y-1,x+1)
-    if x-1>=0 and y-1>=0:
-        f.append((x-1,y-1))
-    if x-1>=0 and y+1<n:
-        f.append((x-1,y+1))
-    if x+1<m and y-1>=0:
-        f.append((x+1,y-1))
-    if x+1<m and y+1<n:
-        f.append((x+1,y-1))
+    # if x-1>=0 and y-1>=0:
+    #     f.append((x-1,y-1))
+    # if x-1>=0 and y+1<n:
+    #     f.append((x-1,y+1))
+    # if x+1<m and y-1>=0:
+    #     f.append((x+1,y-1))
+    # if x+1<m and y+1<n:
+    #     f.append((x+1,y+1))
     if x+2<m:
         f.append((x+2,y))
     if x-2>=0:
@@ -28,30 +28,38 @@ def addTwohopNeighbor(x,y,m,n,f):
 
 def addTwohopWalls(x,y,m,n,f,maze):
     #x+2, x-2, y+2, y-2, (y+1,x+1),(y-1,x-1),(y+1,x-1),(y-1,x+1)
-    if x-1>=0 and y-1>=0:
-        if maze[x-1][y-1]=='#':
-            f.append((x-1,y-1))
-    if x-1>=0 and y+1<n:
-        if maze[x-1][y+1]=='#':
-            f.append((x-1,y+1))
-    if x+1<m and y-1>=0:
-        if maze[x+1][y-1]=='#':
-            f.append((x+1,y-1))
-    if x+1<m and y+1<n:
-        if maze[x+1][y+1]=='#':
-            f.append((x+1,y-1))
+    # if x-1>=0 and y-1>=0:
+    #     if maze[x-1][y-1]=='#':
+    #         if (x-1,y-1) not in f:
+    #             f.append((x-1,y-1))
+    # if x-1>=0 and y+1<n:
+    #     if maze[x-1][y+1]=='#':
+    #         if (x-1,y+1) not in f:
+    #             f.append((x-1,y+1))
+    # if x+1<m and y-1>=0:
+    #     if maze[x+1][y-1]=='#':
+    #         if (x+1,y-1) not in f:
+    #             f.append((x+1,y-1))
+    # if x+1<m and y+1<n:
+    #     if maze[x+1][y+1]=='#':
+    #         if (x+1,y+1) not in f:
+    #             f.append((x+1,y+1))
     if x+2<m:
         if maze[x+2][y]=='#':
-            f.append((x+2,y))
+            if (x+2,y) not in f:
+                f.append((x+2,y))
     if x-2>=0:
         if maze[x-2][y]=='#':
-            f.append((x-2,y))
+            if (x-2,y) not in f:
+                f.append((x-2,y))
     if y+2<n:
         if maze[x][y+2]=='#':
-            f.append((x,y+2))
+            if (x,y+2) not in f:
+                f.append((x,y+2))
     if y-2>=0:
         if maze[x][y-2]=='#':
-            f.append((x,y-2))
+            if (x,y-2) not in f:
+                f.append((x,y-2))
      
     return f
 
@@ -66,27 +74,55 @@ def printMaze(m):
 def createmaze(m,n):
     frontier = []
 
-    maze = [['#']*m]*n
+    maze = []
+    #maze = [['#'] * m for i in range(n)]
+    for x in range(0,m):
+        maze.append([])
+        for y in range(0,n):
+            maze[x].append('#')
     printMaze(maze)
     rand_x = random.randint(0,m-1)
     rand_y = random.randint(0,n-1)
     maze[rand_x][rand_y] = ' '
     frontier = addTwohopNeighbor(rand_x,rand_y,m,n,frontier)
 
-    while len(frontier) > 0:
+    iter = 0
+    while len(frontier)>0:
         neighbors = []
+        print(frontier)
         cellC = frontier[random.randint(0,len(frontier)-1)]
         neighbors = addTwohopNeighbor(cellC[0],cellC[1],m,n,neighbors)
-        free = False
-        while not free:
+        print("neighbors:"+str(neighbors))
+
+        while len(neighbors)>0:
             cellF=neighbors[random.randint(0,len(neighbors)-1)]
             if maze[cellF[0]][cellF[1]]==' ':
-                free=True
+                break
+            else:  
+                neighbors.remove(cellF)
+               
         cellA=[int(abs((cellC[0]+cellF[0])/2)),int(abs((cellC[1]+cellF[1])/2))]
+        print("C:"+str(cellC))
+        print("F:"+str(cellF))
+        print("A:"+str(cellA))
         maze[cellA[0]][cellA[1]]=' '
         frontier.remove(cellC)
         frontier=addTwohopWalls(cellA[0],cellA[1],m,n,frontier,maze)
         printMaze(maze)
+        iter+=1
+
+    # neighbors = []
+    # cellC = frontier[random.randint(0,len(frontier)-1)]
+    # neighbors = addTwohopNeighbor(cellC[0],cellC[1],m,n,neighbors)
+    # free = False
+    # while not free:
+    #     cellF=neighbors[random.randint(0,len(neighbors)-1)]
+    #     if maze[cellF[0]][cellF[1]]==' ':
+    #         free=True
+    # cellA=[int(abs((cellC[0]+cellF[0])/2)),int(abs((cellC[1]+cellF[1])/2))]
+    # maze[cellA[0]][cellA[1]]=' '
+    # frontier.remove(cellC)
+    # frontier=addTwohopWalls(cellA[0],cellA[1],m,n,frontier,maze)
 
     # print(maze)
     printMaze(maze)
