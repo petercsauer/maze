34a846c5f5b0a62873226912b694a20aafbb870c
Peter Sauer <petercsauer@me.com>
2020-09-10 22:29:58 -0500

    fixed creator and added solve capability

--- maze.py
+++ maze.py
@@ -2,79 +2,46 @@ import random
 
 m = 15
 n = 30
+maze=[]
 
 def addTwohopNeighbor(x,y,m,n,f):
     
-    #x+2, x-2, y+2, y-2, (y+1,x+1),(y-1,x-1),(y+1,x-1),(y-1,x+1)
-    # if x-1>=0 and y-1>=0:
-    #     f.append((x-1,y-1))
-    # if x-1>=0 and y+1<n:
-    #     f.append((x-1,y+1))
-    # if x+1<m and y-1>=0:
-    #     f.append((x+1,y-1))
-    # if x+1<m and y+1<n:
-    #     f.append((x+1,y+1))
     global maze
     if x+2<m:
         if maze[x+2][y]=='#':
             if (x+2,y) not in f:
                 f.append((x+2,y))
-                #maze[x+2][y] = '*'
     if x-2>=0:
         if maze[x-2][y]=='#':
             if (x-2,y) not in f:
                 f.append((x-2,y))
-                #maze[x-2][y] = '*'
     if y+2<n:
         if maze[x][y+2]=='#':
             if (x,y+2) not in f:
                 f.append((x,y+2))
-                #maze[x][y+2] = '*'
     if y-2>=0:
         if maze[x][y-2]=='#':
             if (x,y-2) not in f:
                 f.append((x,y-2))
-                #maze[x][y-2] = '*'
 
     return f
 
 def addTwohopWalls(x,y,m,n,f,maze):
-    #x+2, x-2, y+2, y-2, (y+1,x+1),(y-1,x-1),(y+1,x-1),(y-1,x+1)
-    # if x-1>=0 and y-1>=0:
-    #     if maze[x-1][y-1]=='#':
-    #         if (x-1,y-1) not in f:
-    #             f.append((x-1,y-1))
-    # if x-1>=0 and y+1<n:
-    #     if maze[x-1][y+1]=='#':
-    #         if (x-1,y+1) not in f:
-    #             f.append((x-1,y+1))
-    # if x+1<m and y-1>=0:
-    #     if maze[x+1][y-1]=='#':
-    #         if (x+1,y-1) not in f:
-    #             f.append((x+1,y-1))
-    # if x+1<m and y+1<n:
-    #     if maze[x+1][y+1]=='#':
-    #         if (x+1,y+1) not in f:
-    #             f.append((x+1,y+1))
     if x+2<m:        
         if (x+2,y) not in f:
             f.append((x+2,y))
-            #maze[x+2][y] = '*'
     if x-2>=0:
     
         if (x-2,y) not in f:
             f.append((x-2,y))
-            #maze[x-2][y] = '*'
     if y+2<n:
         
         if (x,y+2) not in f:
             f.append((x,y+2))
-            #maze[x][y+2] = '*'
     if y-2>=0:
         
         if (x,y-2) not in f:
             f.append((x,y-2))
-            #maze[x][y-2] = '*'
      
     return f
 
@@ -86,69 +53,70 @@ def printMaze(m):
         print()
     print()
 
+def pickRandSE(m,n):
+    x=random.randint(0,m-1)
+    y=random.randint(0,n-1)
+    x2=random.randint(0,m-1)
+    y2=random.randint(0,n-1)
+    while maze[x][y]=='#':
+        x=random.randint(0,m-1)
+        y=random.randint(0,n-1)
+    while maze[x2][y2]=='#' or (x,y)==(x2,y2):
+        x2=random.randint(0,m-1)
+        y2=random.randint(0,n-1)
+    return (x,y,x2,y2)
+
 def createmaze(m,n):
     frontier = []
 
-
-    #maze = [['#'] * m for i in range(n)]
     for x in range(0,m):
         maze.append([])
         for y in range(0,n):
             maze[x].append('#')
-    printMaze(maze)
-    rand_x = 3#random.randint(0,m-1)
-    rand_y = 4#random.randint(0,n-1)
+    rand_x = random.randint(0,m-1)
+    rand_y = random.randint(0,n-1)
     maze[rand_x][rand_y] = ' '
     frontier = addTwohopNeighbor(rand_x,rand_y,m,n,frontier)
 
-    #iter = 0
     while len(frontier)>0:
         neighbors = []
-        print(frontier)
         cellC = frontier[random.randint(0,len(frontier)-1)]
-        print(cellC)
         neighbors = addTwohopWalls(cellC[0],cellC[1],m,n,neighbors, maze)
-        #print("neighbors:"+str(neighbors))
 
         while len(neighbors)>0:
             cellF=neighbors[random.randint(0,len(neighbors)-1)]
-            print(cellF)
             if maze[cellF[0]][cellF[1]]==' ':
                 break
             else:  
                 neighbors.remove(cellF)
                
         cellA=[int(abs((cellC[0]+cellF[0])/2)),int(abs((cellC[1]+cellF[1])/2))]
-        #print("C:"+str(cellC))
-        #print("F:"+str(cellF))
-        #print("A:"+str(cellA))
+     
         maze[cellA[0]][cellA[1]]=' '
         maze[cellC[0]][cellC[1]]=' '
         frontier.remove(cellC)
         frontier=addTwohopNeighbor(cellC[0],cellC[1],m,n,frontier)
-        printMaze(maze)
-        #iter+=1
+    
+    startAndEnd = pickRandSE(m,n)
+    maze[startAndEnd[0]][startAndEnd[1]]='S'
+    maze[startAndEnd[2]][startAndEnd[3]]='E'
 
-    # neighbors = []
-    # cellC = frontier[random.randint(0,len(frontier)-1)]
-    # neighbors = addTwohopNeighbor(cellC[0],cellC[1],m,n,neighbors)
-    # free = False
-    # while not free:
-    #     cellF=neighbors[random.randint(0,len(neighbors)-1)]
-    #     if maze[cellF[0]][cellF[1]]==' ':
-    #         free=True
-    # cellA=[int(abs((cellC[0]+cellF[0])/2)),int(abs((cellC[1]+cellF[1])/2))]
-    # maze[cellA[0]][cellA[1]]=' '
-    # frontier.remove(cellC)
-    # frontier=addTwohopWalls(cellA[0],cellA[1],m,n,frontier,maze)
 
-    # print(maze)
-    printMaze(maze)
+    return maze
 
 
 
-maze = []
+def writeMaze():
+    f = open("maze.txt", "a")
+    for m in maze:
+        line = ''.join(m)
+        f.write(line)
+        f.write('\n')
+    f.close
+
+
 createmaze(m,n)
+writeMaze()
 # frontier = []
 
 # frontier = addTwohopNeighbor(1,1,m,n,frontier)
--- solve.py
+++ solve.py
@@ -1,3 +1,7 @@
+
+maze = []
+visited=[]
+
 def printMaze(m):
     for row in m:
         for col in row:
@@ -14,10 +18,67 @@ def split(word):
     return [char for char in word]  
 
 def importMaze(mazeString):
-    maze = []
     for x in mazeString:
         line = split(x.rstrip("\n"))
         maze.append(line)
     return maze
-
 printMaze(importMaze(readMaze()))
+print(maze)
+
+
+def findPath(m,n,x,y,goal_x,goal_y):
+    if (x,y) in visited:
+        return False
+    visited.append((x,y))
+    #base case
+    if x < 0 or x > m-1 or y < 0 or y > n-1:
+        print("edge")
+        return False
+    
+    if x == goal_x and y == goal_y:
+        return True
+    print(x,y)
+    if maze[x][y] == '#':
+        print("wall")
+        return False
+
+
+    #mark x,y as part of solution path
+    maze[x][y] = '+'
+
+    #recursive case
+    if findPath(m,n,x,y-1,goal_x,goal_y) == True:
+        return True
+    
+    if findPath(m,n,x+1,y,goal_x,goal_y) == True:
+        return True
+
+    if findPath(m,n,x,y+1,goal_x,goal_y) == True:
+        return True
+
+    if findPath(m,n,x-1,y,goal_x,goal_y) == True:
+        return True
+
+    #unmark x,y
+    maze[x][y] = ' '
+
+    return False
+
+def getStart():
+    for m in range(len(maze)):
+        for n in range(len(maze[m])):
+            if maze[m][n] == 'S':
+                return (m,n)
+
+def getEnd():
+    for m in range(len(maze)):
+        for n in range(len(maze[m])):
+            if maze[m][n] == 'E':
+                return (m,n)
+
+start = getStart()
+end = getEnd()
+
+findPath(15,30,start[0],start[1],end[0],end[1])
+maze[start[0]][start[1]]='S'
+printMaze(maze)
